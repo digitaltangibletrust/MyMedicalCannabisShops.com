d5de42d66ab245ba16a7fc679d6a2ae71d4b6cf9
karlkeefer <karl@karlkeefer.com>
2015-06-11 00:21:13 -0700

    show offer totals above the offer grid

--- views/main/index.js
+++ views/main/index.js
@@ -3,6 +3,10 @@
 var _ = require('lodash');
 
 module.exports.show = function (req, res, next) {
+  res.locals.total = _.reduce(req.app.locals.offerData.offers, function (sum, offer) {
+    return sum += offer.value;
+  }, 0);
+
   res.render('main/index');
 };
 
@@ -10,6 +14,7 @@ module.exports.showPartner = function (req, res, next) {
   var partner = _.find(req.app.locals.offerData.partners, {
     'slug': req.params.slug
   });
+
   if (!partner) {
     return next();
   }

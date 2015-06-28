71b9df8a1dc0abf1783bd16cfd767fd6c1061077
Bolgov Roman <bolgovr@gmail.com>
2015-06-28 23:13:07 +0300

    interstitial popup

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,32 @@
 /* global jQuery, window, document */
 'use strict';
 
-(function($){
+(function ($) {
   $(document).ready(adjustHeights);
   $(window).resize(adjustHeights);
 
-  function adjustHeights(){
+  function adjustHeights() {
     equalizeHeights('.partner-box');
     equalizeHeights('.homepage-offer');
   }
 
-  function equalizeHeights(selector){
-    var heights = $(selector).height('auto').map(function(){
+  function equalizeHeights(selector) {
+    var heights = $(selector).height('auto').map(function () {
       return $(this).height();
     }).get();
 
     var maxHeight = Math.max.apply(null, heights);
     $(selector).height(maxHeight);
   }
+  $('.claim-offer').click(function (e) {
+    e.preventDefault();
+    var offer = $(this).parent();
+    var modal = $('#offerModal');
+    var claimUrl = offer.children('a.btn.claim-offer').attr('href') + '/claim';
+    $('#offerLabel').text(offer.children('h4').text());
+    $('#claim').attr('href', claimUrl);
+    $('#partner').html(offer.children('h5.partner-info').html());
+    $('#offerModal').modal('show');
+  });
 
 })(jQuery);

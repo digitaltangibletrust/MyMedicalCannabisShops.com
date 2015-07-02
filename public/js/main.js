ea6eccce9d70a972aaf74eb325432796779d1adf
Karl Keefer <karl@karlkeefer.com>
2015-07-02 15:49:01 -0700

    fixes for height adjuster

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@
 'use strict';
 
 (function ($) {
-  $(document).ready(adjustHeights);
+  $(window).load(adjustHeights);
   $(window).resize(adjustHeights);
 
   function adjustHeights() {
@@ -18,16 +18,19 @@
     var maxHeight = Math.max.apply(null, heights);
     $(selector).height(maxHeight);
   }
-  $('.claim-offer').click(function (e) {
-    e.preventDefault();
-    var offer = $(this).parent();
-    var modal = $('#offerModal');
-    var claimUrl = offer.children('a.btn.claim-offer').attr('href') + '/claim';
-    $('#offerLabel').text(offer.children('h4').text());
-    $('#claim').attr('href', claimUrl);
-    $('#claimDescription').html(offer.children('.hidden.claim-descr').html());
-    $('#partner').html(offer.children('h5.partner-info').html());
-    $('#offerModal').modal('show');
+
+  $(function(){ //document.ready
+    $('.claim-offer').click(function (e) {
+      e.preventDefault();
+      var offer = $(this).parent();
+      // var modal = $('#offerModal');
+      var claimUrl = offer.children('a.btn.claim-offer').attr('href') + '/claim';
+      $('#offerLabel').text(offer.children('h4').text());
+      $('#claim').attr('href', claimUrl);
+      $('#claimDescription').html(offer.children('.hidden.claim-descr').html());
+      $('#partner').html(offer.children('h5.partner-info').html());
+      $('#offerModal').modal('show');
+    });
   });
 
 })(jQuery);

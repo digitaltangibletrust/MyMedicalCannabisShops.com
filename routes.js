5fcaa1f91aee507de7dcc95748a5e5d50f571f88
Bolgov Roman <bolgovr@gmail.com>
2015-10-13 18:19:21 +0300

    re-introduce merchant's views & add routes for merchant's signup

--- routes.js
+++ routes.js
@@ -7,18 +7,27 @@ var mods = [
   'http',
   'subscribe',
   'offer',
-  'special'
+  'special',
+  'merchant'
 ];
 
 var modLoader = require('./views/index.js');
 var views = modLoader.load(mods);
 
+
+var merchantSignupStack = [multipartMiddleware, views.merchant.processUploads, views.merchant.signup];
+
 module.exports.configure = function (app) {
   views.setupParams(app);
-  app.use(multipartMiddleware);
   app.get('/', views.main.show);
   app.get('/partner/:partner_slug', views.main.showPartner);
+
+  app.get('/i-am-merchant', views.merchant.showForm);
+  app.post('/signup', merchantSignupStack);
+  app.get('/done', views.merchant.done);
+
   app.post('/subscribe', views.subscribe.createSubscription);
+
   app.get('/offer/:offer_id', views.offer.view);
   app.get('/offer/:offer_id/claim', views.offer.claim);
 
